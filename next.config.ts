aaee9ddab7a34c0f22ce4b1cdf2a8ce919054838
Rajat2417 <rajatdeepsingh2417@gmail.com>
2026-04-04 21:26:19 +0530

    Updated config file to handle cloudinary

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
-      // You can add more domains here if you use other image sources
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
     ],
   },
 };
